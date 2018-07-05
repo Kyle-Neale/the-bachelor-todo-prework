cdf87892d762cffef2795d936e8a070c905f99ba
Kyle Neale <kyle.a.neale@gmail.com>
2018-07-05 16:18:54 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,50 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = nil
+  data[season.to_sym].each do |contestant|
+  if contestant[:status] == "Winner"
+    winner = contestant[:name].split(" ").first
+    end
+  end
+  winner
 end
+
+datas = { 
+  "season 30": [
+    {
+      "name":      "Beth Smalls",
+      "age":       "26",
+      "hometown":  "Great Falls, Virginia",
+      "occupation":"Nanny/Freelance Journalist",
+      "status":    "Winner"
+    },
+    {
+      "name":       "Becca Tilley",
+      "age":        "27",
+      "hometown":   "Shreveport, Louisiana",
+      "occupation": "Chiropractic Assistant",
+      "status":     "Eliminated Week 8"
+    }
+  ],
+  "season 29": [
+    {
+      "name":      "Ashley Yeats",
+      "age":       "24",
+      "hometown":  "Denver, Colorado",
+      "occupation":"Dental Assitant",
+      "status":    "Winner"
+    },
+    {
+      "name":       "Sam Grover",
+      "age":        "29",
+      "hometown":   "New York, New York",
+      "occupation": "Entertainer",
+      "status":     "Eliminated Week 6"
+    }
+  ]
+}
+
+puts get_first_name_of_season_winner(datas, "season 30")
+
 
 def get_contestant_name(data, occupation)
   # code here
